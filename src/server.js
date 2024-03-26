a4bd751475a9092615975378850df1a62786c016
Denisa Kita <denisakita7@gmail.com>
2024-03-26 08:42:07 +0100

    adding comments

--- src/server.js
+++ src/server.js
@@ -1,12 +1,56 @@
 import express from "express";
 
 
+let articlesInfo = [
+    {
+        name: 'learn-react',
+        upvotes: 0,
+        comments: []
+    },
+    {
+        name: 'learn-node',
+        upvotes: 0,
+        comments: []
+    },
+    {
+        name: 'mongodb',
+        upvotes: 0,
+        comments: []
+    },
+]
 const app = express();
-app.get('/hello', (req, res) => {
-    res.send('Hello');
+
+app.use(express.json());
+
+app.put('/api/articles/:name/upvote', (req, res) => {
+    const {name} = req.params;
+    const article = articlesInfo.map(
+        a => a.name === name
+    );
+    if (article) {
+        article.upvotes += 1;
+        res.send(`The ${name} article now has ${article.upvotes} upvotes`);
+    } else {
+        res.send(`The article does not exist`);
+    }
+
 });
 
+app.post('/api/articles/:name/comments', (req, res) => {
+    const {name} = req.params;
+    const {postedBy, text} = req.body;
 
-app.listen(8000,()=>{
+    const article = articlesInfo.find(a => a.name === name);
+    if (article) {
+        article.comments.push({postedBy, text});
+        res.send(article.comments);
+
+    } else {
+        res.send(`The article does not exist`);
+
+    }
+
+});
+app.listen(8000, () => {
     console.log('Server is listening on port 8000');
 });
